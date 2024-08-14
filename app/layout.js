f3426e08e8c71dde2104f5d2f7eca0e147feaca4
DairanDoo <your_email@example.com>
2024-08-14 00:07:23 -0400

    Layout en cada página representa la estructura del contenido y ahí se ubica la metadata en una variable en vez del head.

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
 import './globals.css'
 
+// here you put the content that will usually be in the head.
 export const metadata = {
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
 };
 
+// children is the content of the page that is active (in this case page.jsx).
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
